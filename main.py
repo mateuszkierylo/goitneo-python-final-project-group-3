6909a6b4ccc5cc217fc3f5eb2d628d3a123c296a
mateuszkierylo <145701799+mateuszkierylo@users.noreply.github.com>
2024-04-02 10:20:33 +0200

    adds working email check

--- main.py
+++ main.py
@@ -34,14 +34,14 @@ class Address(Field):
 
 class Email(Field):
     def __init__(self, value):
-        self.value = value
+        if self.validate_email(value):
+            self.value = value
+        else:
+            raise ValueError("Invalid email. Please provide a correct email address")
 
     def validate_email(self, email):
         pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-        if re.match(pattern, email):
-            return True
-        else:
-            raise ValueError("Invalid email. Please provide a correct email address")
+        return re.match(pattern, email) is not None
     
 class Birthday(Field):
     def __init__(self, value):
